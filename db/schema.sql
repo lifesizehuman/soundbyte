db89fb2583729a5ca15c7de959fc1dacff94ee27
Slater Combes <combes.slater@gmail.com>
2017-09-27 10:49:57 -0700

    create schema and seeds files

---
+++ db/schema.sql
@@ -0,0 +1,12 @@
+drop database if exists musicArtists_db;
+
+create database musicArtists_db;
+use musicArtists_db;
+
+create table artists
+  (
+  id int auto_increment not null,
+  artist varchar(128) not null,
+  status boolean not null default true,
+  primary key (id)
+);
---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+insert into artists (artist, status)
+  values ("Lady Gaga", false),
+          ("Katy Perry", true),
+          ("Miley Cyrus", false),
+          ("Britney Spears", true),
+          ("Madonna", false);
