d6f2aacc88a8e4fbadba1ca2b85091f8d8393614
Slater Combes <combes.slater@gmail.com>
2017-09-27 10:51:23 -0700

    set up server

---
+++ server.js
@@ -0,0 +1,22 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var methodOverride = require("method-override");
+
+var app = express();
+var port = 8080;
+
+app.use(express.static("public"));
+app.use(methodOverride("_method"));
+app.use(bodyParser.urlencoded({extended: false}));
+
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({defaultLayout: "main"}));
+app.set("view engine", "handlebars");
+
+var routes = require("./controllers/artist_controller.js");
+app.use("/", routes);
+
+app.listen(port, function() {
+  console.log("The server is listening on port " + port);
+});
